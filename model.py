e3bf7c372b5cbab9eb5c1b5c8dbf0e0acb97f22e
mdyer2 <132947537+mdyer2@users.noreply.github.com>
2024-07-11 15:45:13 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,22 @@
+# This file will set up the user interface model creating a class for User 
+# and Message and defining how the passwords will be set through hashing. 
+
+#User model
+class User(db.Model):
+
+
+
+
+  def SetPassword(self, password):
+
+
+  def CheckPassword(self, password):
+
+
+# message model
+class Message(db.Model):
+
+
+
+
+  
