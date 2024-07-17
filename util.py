488969e1ed0c6729f0b209be8b4a714940eaa3c4
mdyer2 <132947537+mdyer2@users.noreply.github.com>
2024-07-17 13:55:10 -0400

    Update util.py
    updated the functions for encryption and decryption

--- util.py
+++ util.py
@@ -5,13 +5,21 @@ from base64 import b64encode, b64decode  # Import base64 encoding and decoding
 import os
 
 #key creation:
-key = 
+key = os.urandom(32)
 
 
 # function to encrypt the message using cipher AES 
 def encrypt_message(message):
-
+    iv = os.urandom(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encrypted = cipher.encrypt(pad(message.encode(), AES.block_size))
+    return b64encode(iv + encrypted).decode()
 
 
 # function to decrypt message using base64 and AES cipher
 def decrypt_message(encrypted_message):
+    encrypted_message = b64decode(encrypted_message)
+    iv = encrypted_message[:16]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted = unpad(cipher.decrypt(encrypted_message[16:]), AES.block_size)
+    return decrypted.decode()
