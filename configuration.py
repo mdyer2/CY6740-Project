02d6ec94c10e170ebf11522df0b2ab2e9e84b644
mdyer2 <132947537+mdyer2@users.noreply.github.com>
2024-07-22 15:01:39 -0400

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,9 +1,9 @@
 import os
 
 # Gets the directory path of the current file
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = '/home/ubuntu/CY6740-Project'
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'SecureChat'  # Secret key for sessions and security NEED TO ADD
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'application.db')  # Database URI for SQLite 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable track modifications for better performance
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'SecureChat'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'application.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
