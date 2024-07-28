2978fa37432743a8a5a5aafa1988023a88179c09
mdyer2 <132947537+mdyer2@users.noreply.github.com>
2024-07-28 16:05:46 -0400

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -4,6 +4,7 @@ import os
 basedir = '/home/ubuntu/CY6740-Project'
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'SecureChat'
+    SECRET_KEY = 'b\x1e\xfa\x8d\xb3\xf5\x0e\x8f\xf6\xe7\xdc\xd0\xd7\xa7\x1b\xc8\xcd\x91\x1c\x11\xbc\xf8\x9a'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'application.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
